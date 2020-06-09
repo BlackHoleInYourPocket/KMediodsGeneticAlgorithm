7bedb1f735f562dcc674270fe1b37b4ee2a31e4b
Seyfi Can Zeyrek <seyficanzeyrek@gmail.com>
2020-06-09 21:24:21 +0300

    First Commits

---
+++ KMediodsGeneticAlgorithm/FileSelection.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace KMediodsGeneticAlgorithm
+{
+    partial class FileSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.populationSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.kNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.iterationNumber = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.AcceptsReturn = true;
+            this.txtPath.AllowDrop = true;
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(18, 50);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPath.Multiline = true;
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(667, 90);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.TextChanged += new System.EventHandler(this.TxtPath_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(697, 50);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(154, 92);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "BROWSE";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // populationSize
+            // 
+            this.populationSize.Location = new System.Drawing.Point(232, 162);
+            this.populationSize.Name = "populationSize";
+            this.populationSize.Size = new System.Drawing.Size(85, 30);
+            this.populationSize.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Population Size     :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "K number               :";
+            // 
+            // kNumber
+            // 
+            this.kNumber.Location = new System.Drawing.Point(232, 216);
+            this.kNumber.Name = "kNumber";
+            this.kNumber.Size = new System.Drawing.Size(85, 30);
+            this.kNumber.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "İteration number    :";
+            // 
+            // iterationNumber
+            // 
+            this.iterationNumber.Location = new System.Drawing.Point(232, 272);
+            this.iterationNumber.Name = "iterationNumber";
+            this.iterationNumber.Size = new System.Drawing.Size(85, 30);
+            this.iterationNumber.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(232, 319);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 58);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Discover";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.resultBox.Location = new System.Drawing.Point(342, 162);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(509, 215);
+            this.resultBox.TabIndex = 9;
+            // 
+            // FileSelection
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(878, 391);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.iterationNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.kNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.populationSize);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FileSelection";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Genetic Algorithm";
+            this.Load += new System.EventHandler(this.FileSelection_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox populationSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox kNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox iterationNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox resultBox;
+    }
+}
+
---
+++ KMediodsGeneticAlgorithm/FileSelection.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KMediodsGeneticAlgorithm
+{
+    public partial class FileSelection : Form
+    {
+        public static List<List<double>> all_data;
+        public static Population population;
+        int p = 0;
+        string file = "";
+        int k = 0;
+        int iterateNumber = 0;
+        public FileSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void FileSelection_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtPath.Text = openFileDialog1.FileName;
+            }
+        }
+        private void TxtPath_TextChanged(object sender, EventArgs e)
+        {
+            file = txtPath.Text;
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = "";
+            p = Convert.ToInt32(populationSize.Text);
+            k = Convert.ToInt32(kNumber.Text);
+            iterateNumber = Convert.ToInt32(iterationNumber.Text);
+
+            population = new Population(p, k);
+            all_data = new List<List<double>>();
+
+            StreamReader sr = new StreamReader(file);
+            string line = "";
+            do
+            {
+                line = sr.ReadLine();
+            } while (line != "@data" && line != "@DATA");
+
+            do
+            {
+                line = sr.ReadLine();
+
+                if (line.Length < 3)
+                    break;
+
+                string[] features = line.Split(',');
+                all_data.Add(new List<double>());
+
+                for (int i = 0; i < features.Length - 1; i++)
+                {
+                    all_data[all_data.Count - 1].Add(Convert.ToDouble(features[i].Replace(".", ",")));
+                }
+
+            } while (!sr.EndOfStream);
+
+            initialization(k);
+
+            for (int i = 0; i < p; i++)
+            {
+                population.data[i].fitness_value = calculateFF(population.data[i]);
+            }
+
+
+            #region Crossovers - Mutation
+            Random rnd = new Random();
+            int mutationNumber = 0;
+            int mutationPercent = 30;
+            for (int crossoverNumber = 0; crossoverNumber < iterateNumber; crossoverNumber++)
+            {
+                int crossIndex;
+                double worstChromosomeFitness = double.MinValue;
+                double bestchrosomeFitness = double.MaxValue;
+                double secondBestChromosomeFitness = double.MaxValue;
+                double newChromosomeBestFitness = double.MaxValue;
+                int bestChromosomeIndex = 0;
+                int secondBestChromosomeIndex = 0;
+                int newBestChromosomeIndex = 0;
+                int minFFIndex = 0;
+                List<Chromosome> newChromosomes = new List<Chromosome>();
+                Chromosome newChromosome = new Chromosome(k);
+                Random rndMutation = new Random();
+                for (int i = 0; i < p; i++)
+                {
+                    if (population.data[i].fitness_value < bestchrosomeFitness)
+                    {
+                        bestchrosomeFitness = population.data[i].fitness_value;
+                        bestChromosomeIndex = i;
+                    }
+                }
+
+                for (int mutant = 0; mutant < population.data.Length; mutant++)
+                {
+                    int mutationChance = rndMutation.Next(100);
+                    //Mutation percent = 0.1;
+                    if (mutationChance < mutationPercent && mutant != bestChromosomeIndex)
+                    {
+                        #region Mutation
+                        mutationNumber++;
+                        Random rndCentroid = new Random();
+                        int mutantK = rndCentroid.Next(1, k);
+                        List<int> selected_index = new List<int>();
+                        int index = 0;
+                        do
+                        {
+                            index = rnd.Next(0, all_data.Count);
+                        } while (selected_index.Count != 0 && selected_index.Contains(index));
+
+                        selected_index.Add(index);
+                        population.data[mutant].centroid[mutantK].lst = new List<double>();
+                        for (int i = 0; i < all_data[0].Count; i++)
+                        {
+                            population.data[mutant].centroid[mutantK].lst.Add(all_data[index][i]);
+                        }
+                        population.data[mutant].fitness_value = calculateFF(population.data[mutant]);
+
+                        #endregion
+                    }
+                }
+
+                for (int i = 0; i < p; i++)
+                {
+                    if ((population.data[i].fitness_value < secondBestChromosomeFitness) && i != bestChromosomeIndex)
+                    {
+                        secondBestChromosomeFitness = population.data[i].fitness_value;
+                        secondBestChromosomeIndex = i;
+                    }
+                }
+
+                newChromosome = new Chromosome(k);
+                crossIndex = rnd.Next(1, k);
+                for (int j = 0; j < crossIndex; j++)
+                {
+                    newChromosome.centroid[j] = population.data[bestChromosomeIndex].centroid[j];
+                }
+
+                for (int j = crossIndex; j < k; j++)
+                {
+                    newChromosome.centroid[j] = population.data[secondBestChromosomeIndex].centroid[j];
+                }
+                newChromosome.fitness_value = calculateFF(newChromosome);
+                newChromosomes.Add(newChromosome);
+
+                for (int i = 0; i < newChromosomes.Count; i++)
+                {
+                    if (newChromosomes[i].fitness_value < newChromosomeBestFitness)
+                    {
+                        newChromosomeBestFitness = newChromosomes[i].fitness_value;
+                        newBestChromosomeIndex = i;
+                    }
+                }
+
+                for (int i = 0; i < p; i++)
+                {
+                    if (population.data[i].fitness_value > worstChromosomeFitness)
+                    {
+                        worstChromosomeFitness = population.data[i].fitness_value;
+                        minFFIndex = i;
+                    }
+                }
+                population.data[minFFIndex] = newChromosomes[newBestChromosomeIndex];
+            }
+            #endregion
+
+            double maxFFAfterCrossover = double.MaxValue;
+            int maxFFIndexAfterCrossover = 0;
+            for (int i = 0; i < p; i++)
+            {
+                if (population.data[i].fitness_value < maxFFAfterCrossover)
+                {
+                    maxFFAfterCrossover = population.data[i].fitness_value;
+                    maxFFIndexAfterCrossover = i;
+                }
+            }
+
+            for (int i = 0; i < population.data[maxFFIndexAfterCrossover].centroid.Length; i++)
+            {
+                resultBox.AppendText(i + 1 + ". Centroid : \t");
+                for (int j = 0; j < population.data[maxFFIndexAfterCrossover].centroid[i].lst.Count; j++)
+                {
+                    resultBox.AppendText(population.data[maxFFIndexAfterCrossover].centroid[i].lst[j] + "\t");
+                }
+                resultBox.AppendText(System.Environment.NewLine);
+            }
+            resultBox.AppendText("Fitness Value : " + population.data[maxFFIndexAfterCrossover].fitness_value);
+            resultBox.AppendText(System.Environment.NewLine);
+            resultBox.AppendText("Mutation Chance: %" + mutationPercent + " and Total Mutation Number : " + mutationNumber);
+
+        }
+        public static void initialization(int k)
+        {
+            Random rnd = new Random();
+
+            for (int p_index = 0; p_index < population.data.Length; p_index++)
+            {
+                List<int> selected_index = new List<int>();
+
+                for (int k_index = 0; k_index < k; k_index++)
+                {
+                    int index = 0;
+
+                    do
+                    {
+                        index = rnd.Next(0, all_data.Count);
+                    } while (selected_index.Count != 0 && selected_index.Contains(index));
+
+                    selected_index.Add(index);
+
+                    for (int i = 0; i < all_data[0].Count; i++)
+                    {
+                        population.data[p_index].centroid[k_index].lst.Add(all_data[index][i]);
+                    }
+                }
+            }
+        }
+        public static double calculateFF(Chromosome chromosome)
+        {
+            List<int> clusters = new List<int>();
+
+            for (int i = 0; i < all_data.Count; i++)
+            {
+                double min_val = Double.MaxValue;
+                int min_index = -1;
+
+                for (int j = 0; j < chromosome.centroid.Length; j++)
+                {
+                    double resultED = calculateED(all_data[i], chromosome.centroid[j].lst);
+
+                    if (min_val > resultED)
+                    {
+                        min_val = resultED;
+                        min_index = j;
+                    }
+                }
+
+                clusters.Add(min_index);
+            }
+            return calculateSSE(clusters, chromosome);
+        }
+
+        public static double calculateED(List<double> lst1, List<double> lst2)
+        {
+            double result = 0;
+
+            for (int i = 0; i < lst1.Count; i++)
+            {
+                result += Math.Pow(lst1[i] - lst2[i], 2);
+            }
+            return Math.Sqrt(result);
+        }
+
+
+        public static double calculateSSE(List<int> clusters, Chromosome chr)
+        {
+            double result = 0;
+
+            for (int i = 0; i < clusters.Count; i++)
+            {
+                result += calculateED(chr.centroid[clusters[i]].lst, all_data[i]);
+            }
+            return result;
+        }
+
+    }
+
+    public class Centroid
+    {
+        public List<double> lst = new List<double>();
+    }
+    public class Chromosome
+    {
+        public Centroid[] centroid;
+        public double fitness_value;
+
+        public Chromosome(int k)
+        {
+            centroid = new Centroid[k];
+            for (int i = 0; i < k; i++)
+            {
+                centroid[i] = new Centroid();
+            }
+        }
+    }
+
+    public class Population
+    {
+
+        public Chromosome[] data;
+
+
+        public Population(int p, int k)
+        {
+            data = new Chromosome[p];
+            for (int i = 0; i < p; i++)
+            {
+                data[i] = new Chromosome(k);
+            }
+        }
+
+    }
+}
